19b524467231a570f174e7fb08db98cde9f47ec1
Ranjan Mishra <ranjan.mishra.dev@gmail.com>
2026-04-17 20:05:49 +0530

    feat: integrate Supabase database in backend

--- yaarspace-backend/src/app.js
+++ yaarspace-backend/src/app.js
@@ -1,11 +1,23 @@
 import express from 'express'
+import cors from 'cors';
 const app = express();
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+)
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({ message: 'YaarSpace API is running...' });
 });
 
+app.get('/api/test', (req, res) => {
+  res.json({ success: true, message: 'API working for frontend' });
+});
 
 export default app;
--- yaarspace-backend/src/db/supabaseClient.js
+++ yaarspace-backend/src/db/supabaseClient.js
@@ -1,4 +1,5 @@
-const { createClient } = require('@supabase/supabase-js');
+// const { createClient } = require('@supabase/supabase-js');
+import {createClient} from '@supabase/supabase-js'
 
 if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_ROLE_KEY) {
   throw new Error("Missing Supabase credentials");
