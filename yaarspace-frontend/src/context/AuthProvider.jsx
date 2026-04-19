866a0262e7c60b406bf61d2e126d1dd8a187b5f5
Ranjan Mishra <ranjan.mishra.dev@gmail.com>
2026-04-19 21:17:25 +0530

    feat: implement auth state management with context API

---
+++ yaarspace-frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,47 @@
+import { useContext, useEffect, useState } from "react";
+
+import { supabase } from "@/services/supabaseClient";
+import { createContext } from "react";
+
+export const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setUser(session?.user ?? null);
+      setLoading(false);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user ?? null);
+      setLoading(false);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, loading, handleLogout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within AuthProvider");
+  }
+  return context;
+};
