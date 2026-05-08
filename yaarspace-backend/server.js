164d7f6b2042960631e25d9e4d17d31d4cbb861f
Ranjan Mishra <ranjan.mishra.dev@gmail.com>
2026-05-08 23:48:33 +0530

    chore: change port number

--- yaarspace-backend/server.js
+++ yaarspace-backend/server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 import server from './src/app.js';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
